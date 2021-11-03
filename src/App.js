504642a9b6a043b622e3a0a897f846b9f4b4dcef
Brandon <brandonbgrayson@gmail.com>
2021-11-03 01:34:02 -0400

    Added Chakra Ui and Navbar Template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "../src/components/Navbar"
+import { ChakraProvider } from "@chakra-ui/react"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+   <ChakraProvider>
+     <Navbar />
+   </ChakraProvider>
   );
 }
 
